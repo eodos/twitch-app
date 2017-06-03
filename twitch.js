fd998f4e2b70d898bc9b06f860a076fae394a803
David Paul Pena <dpaul@nyu.edu>
2017-06-03 18:45:57 -0400

    Add input box to add streamers

--- twitch.js
+++ twitch.js
@@ -6,14 +6,14 @@ const channels = [
   "storbeck",
   "habathcx",
   "RobotCaleb",
-  "noobs2ninjas",
-  "brunofin",
-  "comster404",
+  "xLanne",
+  "Claritytx",
+  "summit1g",
   "anniefuchsia"
 ];
 let renders = [];
 
-const getAccountsInfo = () => {
+const getAccountsInfo = channels => {
   let promises = [];
   const query = "https://wind-bow.glitch.me/twitch-api/streams/";
   $.each(channels, (index, channel) => {
@@ -51,7 +51,7 @@ const parseResponse = response => {
 
   data.stream == null ? (connected = false) : (connected = true);
   connected
-    ? (game = "Playing ... " + data.stream.channel.game)
+    ? (game = `Playing ... ${data.stream.channel.game}`)
     : (game = "OFFLINE");
   connected
     ? (logo = data.stream.channel.logo)
@@ -60,21 +60,26 @@ const parseResponse = response => {
   connected ? (url = data.stream.channel.url) : (url = "");
 
   html =
-    "<div class='channel' id='channel_" +
-    channel +
-    "'><a target='_blank' href='" +
-    url +
-    "'><table style='width:100%'><tr><td style='width:120px'><img height=100 class='logo' src='" +
-    logo +
-    "'/></td><td><h4>" +
-    channel +
-    "</h4>" +
-    game +
-    "<br/>" +
-    status +
-    "</td></tr></table></a><a class='close' style='margin: -67px -50px 0px 0px; color: black' onclick='deleteChannel(\"" +
-    channel +
-    "\")'>&times;</a></div>";
+    `<div class='channel' id='channel_${channel}'>
+      <a target='_blank' href='${url}'>
+        <table style='width:100%'>
+          <tr>
+            <td style='width:120px'>
+              <img height=100 class='logo' src='${logo}'/>
+            </td>
+            <td>
+              <h4>${channel}</h4>
+              ${game}
+              <br/>
+              ${status}
+            </td>
+          </tr>
+        </table>
+      </a>
+      <a class='close' style='margin: -67px -50px 0px 0px; color: black'
+        onclick='deleteChannel("${channel}")'>&times;
+      </a>
+    </div>`;
 
   const output = {
     online: connected,
@@ -89,13 +94,13 @@ const deleteChannel = channel => {
   $.each(renders, (index, component) => {
     if (component.name == channel) {
       renders.splice(index, 1);
-      $("#channel_" + channel).remove();
+      $(`#channel_${channel}`).remove();
       return false;
     }
   });
 };
 
-getAccountsInfo();
+getAccountsInfo(channels);
 $("#showAll").click(() => {
   render(true, true);
 });
@@ -104,4 +109,10 @@ $("#showOnline").click(() => {
 });
 $("#showOffline").click(() => {
   render(false, true);
+});
+$("#addChannel").on('keyup', (e) => {
+    if (e.keyCode == 13) {
+      getAccountsInfo([$("#addChannel")[0].value]);
+      $("#addChannel")[0].value = "";
+    }
 });
